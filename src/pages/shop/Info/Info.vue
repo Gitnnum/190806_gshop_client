1253deea0e60664d8c8b44800dcbbb421329cb4b
liule <liule@qq.com>
2019-08-12 21:01:56 +0800

    商家info完成

--- src/pages/shop/Info/Info.vue
+++ src/pages/shop/Info/Info.vue
@@ -1,37 +1,25 @@
 <template>
-  <div class="shop-info">
+  <div class="shop-info" ref="shopInfo">
     <div class="info-content">
       <section class="section">
         <h3 class="section-title">配送信息</h3>
         <div class="delivery">
           <div>
-            <span class="delivery-icon">硅谷专送</span>
-            <span>由商家配送提供配送，约30分钟送达，距离100m</span>
+            <span class="delivery-icon">{{info.description}}</span>
+            <span>由商家配送提供配送，约{{info.deliveryTime}}分钟送达，距离{{info.distance}}</span>
           </div>
-          <div class="delivery-money">配送费￥3</div>
+          <div class="delivery-money">配送费￥{{info.deliveryPrice}}</div>
         </div>
       </section>
       <div class="split"></div>
       <section class="section">
         <h3 class="section-title">活动与服务</h3>
         <div class="activity">
-          <div class="activity-item activity-green">
+          <div class="activity-item" :class="supportClasses[support.type]" v-for="(support, index) in info.supports" :key="index">
             <span class="content-tag">
-              <span class="mini-tag">首单</span>
+              <span class="mini-tag">{{support.name}}</span>
             </span>
-            <span class="activity-content">新用户下单立减17元(不与其它活动同享)</span>
-          </div>
-          <div class="activity-item activity-red">
-            <span class="content-tag">
-              <span class="mini-tag">满减</span>
-            </span>
-            <span class="activity-content">满35减19，满65减35</span>
-          </div>
-          <div class="activity-item activity-orange">
-            <span class="content-tag">
-              <span class="mini-tag">特价</span>
-            </span>
-            <span class="activity-content">【立减19.5元】欢乐小食餐</span>
+            <span class="activity-content">{{support.content}}</span>
           </div>
         </div>
       </section>
@@ -39,19 +27,10 @@
 
       <section class="section">
         <h3 class="section-title">商家实景</h3>
-        <div class="pic-wrapper">
+        <div class="pic-wrapper" ref="picWrapper">
           <ul class="pic-list">
-            <li class="pic-item">
-              <img width="120" height="90" src="https://fuss10.elemecdn.com/f/7f/d1422ec824a0a9d1fb879c57ab533jpeg.jpeg">
-            </li>
-            <li class="pic-item">
-              <img width="120" height="90" src="https://fuss10.elemecdn.com/f/7f/d1422ec824a0a9d1fb879c57ab533jpeg.jpeg">
-            </li>
-            <li class="pic-item">
-              <img width="120" height="90" src="https://fuss10.elemecdn.com/f/7f/d1422ec824a0a9d1fb879c57ab533jpeg.jpeg">
-            </li>
-            <li class="pic-item">
-              <img width="120" height="90" src="https://fuss10.elemecdn.com/f/7f/d1422ec824a0a9d1fb879c57ab533jpeg.jpeg">
+            <li class="pic-item" v-for="(pic, index) in info.pics" :key="index">
+              <img width="120" height="90" :src="pic">
             </li>
           </ul>
         </div>
@@ -63,19 +42,19 @@
         <ul class="detail">
           <li>
             <span class="bold">品类</span>
-            <span>包子粥店, 简餐</span>
+            <span>{{info.category}}</span>
           </li>
           <li>
             <span class="bold">商家电话</span>
-            <span>18501081111</span>
+            <span>{{info.phone}}</span>
           </li>
           <li>
             <span class="bold">地址</span>
-            <span>北京市昌平区回龙观44号</span>
+            <span>{{info.address}}</span>
           </li>
           <li>
             <span class="bold">营业时间</span>
-            <span>09:35-21:00</span>
+            <span>{{info.workTime}}</span>
           </li>
         </ul>
       </section>
@@ -83,7 +62,44 @@
   </div>
 </template>
 <script type="text/ecmascript-6">
+  import { mapState } from 'vuex'
+  import BScroll from 'better-scroll'
   export default {
+    data () {
+      return {
+        supportClasses: ['activity-green', 'activity-red', 'activity-orange']
+      }
+    },
+    mounted () {
+      if(this.info.name){
+        new BScroll(this.$refs.shopInfo,{
+          click: true
+        })
+        new BScroll (this.$refs.picWrapper,{
+          click: true,
+          scrollX: true
+        })
+      }
+    },
+    computed: {
+      ...mapState({
+        info: state => state.shop.info
+      })
+    },
+    watch: {
+      info () {
+        this.$nextTick(() => {
+          new BScroll(this.$refs.shopInfo,{
+            click: true
+          })
+
+          new BScroll (this.$refs.picWrapper,{
+            click: true,
+            scrollX: true
+          })
+        })
+      }
+    },
   }
 </script>
 
@@ -172,6 +188,7 @@
         margin-top 16px
         .pic-list
           font-size: 0
+          float left
           .pic-item
             display: inline-block
             margin-right: 6px
